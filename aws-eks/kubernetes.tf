e99c697d5b92d65c934d81c15999ef7272923f34
Lynda Bounif <l_bounif@esi.dz>
2024-07-04 18:42:27 -0400

    update kubernetes provider

--- aws-eks/kubernetes.tf
+++ aws-eks/kubernetes.tf
@@ -1,7 +1,7 @@
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster
-  token                  = data.aws_eks_cluster_auth.cluster
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster_auth.cluster)
+provider "kubernetes" { 
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
 resource "kubernetes_namespace" "test" {
--- aws-eks/main.tf
+++ aws-eks/main.tf
@@ -55,13 +55,13 @@ module "eks" {
   }
 }
 output "cluster_id" {
-  value = module.eks.cluster_id
+  value = module.eks.cluster_name
 }
 
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
